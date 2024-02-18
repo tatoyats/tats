fcae9562bac5b84f0b6d512ff2f3e3621e8f59d1
tatoyats <113349112+tatoyats@users.noreply.github.com>
2024-02-17 21:06:36 -0800

    Delete CRUD10 directory

--- CRUD10/delete.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include("db_conn.php");
-$id = $_GET["id"];
-$sql = "DELETE FROM `user` WHERE id = $id";
-$result = mysqli_query($conn, $sql);
-if($result){
-    header("Location: index.php?msg=Record Deleted");
-}
-?>
