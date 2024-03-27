e9cddee172a78809e9d628c125b24bdb84bdcb96
tatoyats <113349112+tatoyats@users.noreply.github.com>
2024-03-27 05:47:53 -0800

    Add files via upload

---
+++ tailsidebar/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sidebar</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;800&display=swap"
+    rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+</head>
+<body class="bg-blue-600 font-[Poppins]">
+    <span class="absolute text-white text-4x1 top-5 left-4 cursor-pointer" onclick="Openbar()">
+        <i class="bi bi-filter-left px-2 bg-gray-900 rounded-md"></i>
+    </span>
+<div class="container">  
+    <div class="sidebar fixed top-0 bottom-0 lg:left-0 left-[-300px] duration-1000
+    p-2 w-[300px] overflow-y-quto text-center bg-gray-900 shadow h-screen">
+
+        <div class="text-gray-100 text-xl container">
+            <div class="p-2.5 mt-1 flex items-center rounded-md">
+                <i class="bi bi-app-indicator px-2 py-1 bg-blue-600 rounded-md"></i>
+                <h1 class="text-[15px] ml-3 text-xl text-gray-200 font-bold">Tailwindbar</h1>
+                <i class="bi bi-x ml-20 cursor-pointer lg:hidden" onclick="Openbar()"></i>
+            </div>
+            <hr class="my-2 text-gray-600">
+
+            <div>
+                <div class="p-2.5 mt.3 flex items-center rounded-md
+                px-4 duration-300 cursor-pointer bg-gray-700">
+                    <i class="bi bi-search text-sm"></i>
+                    <input class="text-[15px] ml-4 w-full bg-transparent focus:outline-none" placeholder="Search">
+                </div>
+
+                <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600">
+                    <i class="bi bi-house-door-fill"></i>
+                    <span class="text-[15px] ml-4 text-gray-200">Home</span>
+                </div>
+
+                <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600">
+                    <i class="bi bi-bookmark-fill"></i>
+                    <span class="text-[15px] ml-4 text-gray-200">Bookmark</span>
+                </div>
+
+                <hr class="my-4 text-gray-600">
+                <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600">
+                    <i class="bi bi-envelope-fill"></i>
+                    <span class="text-[15px] ml-4 text-gray-200">Messages</span>
+                </div>
+
+                <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600">
+                    <i class="bi bi-chat-left-text-fill"></i>
+                    <div class="flex justify-between w-full items-center" onclick="dropDown()">
+                        <span class="text-[15px] ml-4 text-gray-200">Chatbox</span>
+                        <span class="text-sm rotate-180" id="arrow">
+                            <i class="bi bi-chevron-down"></i>
+                        </span>
+                    </div>
+                </div>
+                <div class="leading-7 text-left text-sm font-thin mt-2 w-4/5 mx-auto" id="submenu">
+                    <h1 class="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">Social</h1>
+                    <h1 class="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">Personal</h1>
+                    <h1 class="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">Friends</h1>
+                </div>
+                <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600">
+                    <i class="bi bi-box-arrow-in-right"></i>
+                    <span class="text-[15px] ml-4 text-gray-200">Logout</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main">
+        <div class="pl-96 py-8 w-full relative">
+            <div class="shadow overflow-hidden rounded border-b border-gray-200">
+                <table class="min-w-full bg-white">
+                    <thead class="bg-gray-800 text-white">
+                        <tr>
+                            <th class="w-1/4 text-left py-3 px-4 uppercase font-semibold text-sm">ID</th>
+                            <th class="w-1/4 text-left py-3 px-4 uppercase font-semibold text-sm">From</th>
+                            <th class="w-1/4 text-left py-3 px-4 uppercase font-semibold text-sm">To</th>
+                            <th class="w-1/4 text-left py-3 px-4 uppercase font-semibold text-sm">Date to Depart</th>
+                            <th class="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Airline</th>
+                        </tr>
+                    </thead>
+                    <?php 
+                    include("db_conn.php");
+                    $sql = "SELECT * FROM `flight`";
+                    $result = mysqli_query($conn, $sql);
+                    while ($row = mysqli_fetch_assoc($result)){
+                    ?>
+                    <tbody class="text-gray-700">
+                        <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                            <td class="w-1/4 text-left text-sm py-3 px-4"><?php echo $row['id']?></td>
+                            <td class="w-1/4 text-left text-sm py-3 px-4"><?php echo $row['from_place']?></td>
+                            <td class="w-1/4 text-left text-sm py-3 px-4"><?php echo $row['to_place']?></td>
+                            <td class="w-1/4 text-left text-sm py-3 px-4"><?php echo $row['date']?></td>
+                            <td class="w-1/4 text-left text-sm py-3 px-4"><?php echo $row['plane']?></td>
+                        </tr>
+                    </tbody>
+                    <?php }?>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+  
+    
+    <script>
+        function dropDown(){
+            document.querySelector('#submenu').classList.toggle('hidden')
+            document.querySelector('#arrow').classList.toggle('rotate-0')
+        }
+        dropDown()
+
+        function Openbar(){
+            document.querySelector('.sidebar').classList.toggle('left-[-300px]')
+        }
+    </script>
+    
+</body>
+</html>
